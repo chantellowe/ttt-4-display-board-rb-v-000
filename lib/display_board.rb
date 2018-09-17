bc7be44954466575f39e5057cce808bc66b95873
Chantel Lowe <chantellowe17@gmail.com>
2018-09-17 21:07:34 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,8 @@ board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board(board)
   puts "   "+"|"+"   "+"|"+"   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   "+"|"+"   "+"|"+"   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   "+"|"+"   "+"|"+"   "
 end
 display_board(board)
